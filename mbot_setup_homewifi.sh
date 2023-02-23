832aafb995f2b86fcf99f15048ddeac4901b1303
Peter Gaskell <pgaskell@umich.edu>
2023-02-22 23:00:57 -0500

    fixes

--- mbot_setup_homewifi.sh
+++ mbot_setup_homewifi.sh
@@ -1,7 +1,13 @@
-MyWifi="$1"
-MyWifiPassword="$2"
+# Prompt the user for the Wi-Fi SSID
+read -p "Enter the Wi-Fi SSID: " MyWifi
+
+# Prompt the user for the Wi-Fi password, hiding the input
+read -s -p "Enter the Wi-Fi password: " MyWifiPassword
+echo ""
 
 sudo nmcli connection add type wifi ifname wlan0 con-name $MyWifi autoconnect yes ssid $MyWifi
 sudo nmcli connection modify $MyWifi wifi-sec.key-mgmt wpa-psk
 sudo nmcli connection modify $MyWifi wifi-sec.psk $MyWifiPassword
 nmcli connection modify $MyWifi connection.autoconnect-priority 5
+
+echo "Connection Saved"
