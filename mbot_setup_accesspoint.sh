cdfcc4fedccde5e8c10b68e3c168181b8828717f
Peter Gaskell <pgaskell@umich.edu>
2023-02-22 21:06:36 -0500

    1

--- mbot_setup_accesspoint.sh
+++ mbot_setup_accesspoint.sh
@@ -1,7 +1,6 @@
 mbot_hostname=$(hostname)
-MyAccessPoint = "$mbot_hostname-AP"
-MyPassword = "iloverobots"
-
+MyAccessPoint="$mbot_hostname-AP"
+MyPassword="iloverobots"
 sudo nmcli connection add type wifi ifname '*' con-name $MyAccessPoint autoconnect no ssid $MyAccessPoint
 sudo nmcli connection modify $MyAccessPoint 802-11-wireless.mode ap 802-11-wireless.band ac ipv4.method shared
 sudo nmcli connection modify $MyAccessPoint wifi-sec.key-mgmt wpa-psk
