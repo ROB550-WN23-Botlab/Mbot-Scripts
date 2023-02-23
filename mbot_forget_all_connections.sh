49814e544915ed722236af03a0152f273270c1dd
Peter Gaskell <pgaskell@umich.edu>
2023-02-22 22:43:59 -0500

    fixes

--- mbot_forget_all_connections.sh
+++ mbot_forget_all_connections.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 # Get a list of all connections and their UUIDs
-connections=$(nmcli connection show | awk '{print $2}')
+connections=$(nmcli connection show | awk 'NR>1{print $1}')
 
-# Loop through the connections and forget them
-for conn in $connections; do
-    nmcli connection delete uuid "$conn"
+# Iterate through list of WiFi connection names
+for connection in $connections
+do
+  # Skip over the "Wired" connection
+  if [ "$connection" = "Wired" ]; then
+    echo "Skipping Wired connection"
+    continue
+  fi
+  
+  echo "Deleting WiFi connection: $connection"
+  nmcli connection delete $connection
 done
