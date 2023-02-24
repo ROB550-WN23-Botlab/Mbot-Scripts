c41477f32e8ea4a8d934879792b1462d20a9489f
pgaskell <pgaskell@umich.edu>
2023-02-24 14:14:41 -0500

    made service for starting up

--- setup_from_mbot_config.py
+++ setup_from_mbot_config.py
@@ -38,7 +38,7 @@ wifi_status = os.popen("nmcli -t -f NAME,DEVICE,STATE c show --active").read().s
 if wifi_status:
     for line in wifi_status.split('\n'):
         name, device, state = line.split(':')
-        if device == 'wlan0' and state == 'activated':
+        if device == 'wlan0' and state == 'activated' and name != 'mbot_wifi_ap':
             wifi_active = True
             break
 
@@ -46,26 +46,36 @@ if wifi_active:
     # Already connected to  WiFi network
     print("Connected to a WiFi network... Done.")
 else:
+    print("No connection active, attempting to connect to home network...")
+    # check if we see our home wifi network
     available_networks = []
+    known_networks = []
     scan_output = os.popen("sudo nmcli dev wifi list").read().split('\n')
     for line in scan_output:
         if len(line.strip()) > 0 and not line.startswith("IN-USE"):
-            available_networks.append(line.strip().split()[0])
-            print(line.strip().split()[0])
-    home_wifi_exists = False
+            available_networks.append(line.strip().split()[1])
     if home_wifi_ssid in available_networks:
-        # Set up home WiFi connection  
-        for line in os.popen("nmcli connection show").readlines():
-            print(line)
-            if home_wifi_ssid in line:
-                home_wifi_exists = True
-                break
-
-    if home_wifi_exists:
-        # Connect to home WiFi network
-        os.system(f"sudo nmcli connection up '{home_wifi_ssid}' password '{home_wifi_password}'")
-        print("Connected to home WiFi network.")
+        print("Found WiFi network, checking for network profile...")
+        output = os.popen("nmcli connection show").read().split('\n')
+        for line in output:
+            if len(line.strip()) > 0:
+                known_networks.append(line.strip().split()[0])
+        if home_wifi_ssid in known_networks:
+            print("Found network profile. Connecting...")
+            # Connect to home WiFi network
+            os.system(f"sudo nmcli connection up '{home_wifi_ssid}'")
+            print("Connected to home WiFi network.")
+        else:
+            print("No network profile found. Adding new profile...")
+            # Adding network config and connecting
+            os.system(f"sudo nmcli connection add type wifi ifname wlan0 con-name '{home_wifi_ssid}' autoconnect yes ssid '{home_wifi_ssid}'")
+            os.system(f"sudo nmcli connection modify '{home_wifi_ssid}' wifi-sec.key-mgmt wpa-pask")
+            os.system(f"sudo nmcli connection modify '{home_wifi_ssid}' wifi-sec.psk '{home_wifi_password}'")
+            os.system(f"sudo nmcli connection up '{home_wifi_ssid}'")
+            print("Connected to home WiFi network.")
+        wifi_active = True
     else:
+        print("No connections available. Starting Wifi AP...")
         # Check if the access point already exists
         ap_exists = False
         for line in os.popen("nmcli connection show").readlines():
@@ -74,16 +84,18 @@ else:
                 break
 
         if not ap_exists:
+            print("Access point profile does not exist. Creating now...")
             # Configure Network Manager to create a WiFi access point
             os.system(f"sudo nmcli connection add type wifi ifname '*' con-name mbot_wifi_ap autoconnect no ssid {ap_ssid}")
             os.system("sudo nmcli connection modify mbot_wifi_ap 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared")
             os.system(f"sudo nmcli connection modify mbot_wifi_ap wifi-sec.key-mgmt wpa-psk wifi-sec.psk {ap_password}")
-            os.system("sudo nmcli connection modify mbot_wifi_ap ipv4.address 192.168.1.1/24 ipv4.dns '8.8.8.8 8.8.4.4'")
+            os.system("sudo nmcli connection modify mbot_wifi_ap ipv4.address 192.168.1.1/24 ipv4.gateway 192.168.1.1")
             time.sleep(10.0)
             os.system("sudo nmcli connection up mbot_wifi_ap")
             print("Access point created successfully.")
         else:
-            print("Access point already exists, not created.")
-
-        # Turn on WiFi radio
-        os.system("sudo nmcli radio wifi on")
+            print("Access point profile already exists...")
+            os.system("sudo nmcli connection up mbot_wifi_ap")
+            print("Access point created successfully.")
+if wifi_active == True:
+    os.system("/home/pi/Installers/update_IP.sh")
